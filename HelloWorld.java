d87b0c9a3a0ae4331d2168ebd734689e89940cc3
wspasindupanthaka <wpasindupanthaka@gmail.com>
2020-10-18 11:00:02 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("*****--Running from Jenkins Poll SCM--*****");
+        System.out.println("*****--Running from Jenkins Poll SCM Edit Kara Ban--*****");
     }
 }
